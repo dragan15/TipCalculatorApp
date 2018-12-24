5c579737fd8595280ef138987a3b2582614b9986
temp <temp@temps-MacBook-Pro.local>
2018-12-23 23:06:21 -0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by temp on 12/23/18.
+//  Copyright © 2018 temp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
